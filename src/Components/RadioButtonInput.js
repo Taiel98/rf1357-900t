a3121d7bfe5baeb7b6de09d983e37f8e710d55f4
Taiel98 <taielrodrigok@gmail.com>
2025-07-31 16:42:25 -0300

    corrigiendo registro 2

--- src/Components/RadioButtonInput.js
+++ src/Components/RadioButtonInput.js
@@ -1,8 +1,15 @@
+
 import React from "react";
 
 function RadioButtonInput(props){
     return(
-        <input type="radio" name={props} value={props}/>
+        <input 
+            type="radio" 
+            name={props.name} 
+            value={props.value}
+            onChange={props.onChange}
+            checked={props.checked}
+        />
     )
 }
 
--- src/Routes/Registro2.js
+++ src/Routes/Registro2.js
@@ -7,9 +7,34 @@ import RadioButtonInput from "../Components/RadioButtonInput"
 
 function Registro2(){
     const [beneficio, setBeneficio] = useState('');
+    const [transporte, setTransporte] = useState('');
+    const [ley27718, setLey27718] = useState('');
+    const [teletrabajo, setTeletrabajo] = useState('');
+    const [militar, setMilitar] = useState('');
+    const [transporteTerrestre, setTransporteTerrestre] = useState('');
     
     const handleSelectChange = (e) => {
         setBeneficio(e.target.value);
+    };
+
+    const handleTransporteChange = (e) => {
+        setTransporte(e.target.value);
+    };
+
+    const handleLey27718Change = (e) => {
+        setLey27718(e.target.value);
+    };
+
+    const handleTeletrabajoChange = (e) => {
+        setTeletrabajo(e.target.value);
+    };
+
+    const handleMilitarChange = (e) => {
+        setMilitar(e.target.value);
+    };
+
+    const handleTransporteTerrestreChange = (e) => {
+        setTransporteTerrestre(e.target.value);
     };
 
     return(
@@ -38,40 +63,90 @@ function Registro2(){
             </SelectInput>
             <h3>7 ¿Desarrolla actividad de transporte de larga distancia? (NO CT 40/1989)</h3>
             <label>
-                <input type="radio" name="transporte" value="si"/> Sí
+                <RadioButtonInput 
+                    name="transporte" 
+                    value="si" 
+                    checked={transporte === 'si'}
+                    onChange={handleTransporteChange}
+                /> Sí
             </label>
             <label>
-                <input type="radio" name="transporte" value="no"/> No
+                <RadioButtonInput 
+                    name="transporte" 
+                    value="no" 
+                    checked={transporte === 'no'}
+                    onChange={handleTransporteChange}
+                /> No
             </label>
             <h3>8 ¿El trabajador posee el beneficio promocional de la Ley N° 27.424?</h3>
             <NumberInput value="0" readOnly={true}/>
             <h3>9 ¿El trabajador posee el beneficio de la Ley N° 27.718?</h3>
             <label>
-                <input type="radio" name="ley27718" value="si"/> Sí
+                <RadioButtonInput 
+                    name="ley27718" 
+                    value="si" 
+                    checked={ley27718 === 'si'}
+                    onChange={handleLey27718Change}
+                /> Sí
             </label>
             <label>
-                <input type="radio" name="ley27718" value="no"/> No
+                <RadioButtonInput 
+                    name="ley27718" 
+                    value="no" 
+                    checked={ley27718 === 'no'}
+                    onChange={handleLey27718Change}
+                /> No
             </label>
             <h3>10 ¿El trabajador labora bajo el régimen de teletrabajo - Ley 27.555?</h3>
             <label>
-                <input type="radio" name="teletrabajo" value="si"/> Sí
+                <RadioButtonInput 
+                    name="teletrabajo" 
+                    value="si" 
+                    checked={teletrabajo === 'si'}
+                    onChange={handleTeletrabajoChange}
+                /> Sí
             </label>
             <label>
-                <input type="radio" name="teletrabajo" value="no"/> No
+                <RadioButtonInput 
+                    name="teletrabajo" 
+                    value="no" 
+                    checked={teletrabajo === 'no'}
+                    onChange={handleTeletrabajoChange}
+                /> No
             </label>
             <h3>11 ¿El trabajador es personal militar en actividad - Ley 19.101?</h3>
             <label>
-                <input type="radio" name="militar" value="si"/> Sí
+                <RadioButtonInput 
+                    name="militar" 
+                    value="si" 
+                    checked={militar === 'si'}
+                    onChange={handleMilitarChange}
+                /> Sí
             </label>
             <label>
-                <input type="radio" name="militar" value="no"/> No
+                <RadioButtonInput 
+                    name="militar" 
+                    value="no" 
+                    checked={militar === 'no'}
+                    onChange={handleMilitarChange}
+                /> No
             </label>
             <h3>12 ¿El trabajador desarrolla la actividad de transporte TERRESTRE de larga distancia bajo el convenio 40/1989?</h3>
             <label>
-                <input type="radio" name="transporteTerrestre" value="si"/> Sí
+                <RadioButtonInput 
+                    name="transporteTerrestre" 
+                    value="si" 
+                    checked={transporteTerrestre === 'si'}
+                    onChange={handleTransporteTerrestreChange}
+                /> Sí
             </label>
             <label>
-                <input type="radio" name="transporteTerrestre" value="no"/> No
+                <RadioButtonInput 
+                    name="transporteTerrestre" 
+                    value="no" 
+                    checked={transporteTerrestre === 'no'}
+                    onChange={handleTransporteTerrestreChange}
+                /> No
             </label>
             <br/>
         </>
