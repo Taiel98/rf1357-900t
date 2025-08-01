09a1f8560681cd68baaabf0b56f8face81d6b704
Taiel98 <taielrodrigok@gmail.com>
2025-08-01 10:57:06 -0300

    cosas

--- src/Routes/Registro5.js
+++ src/Routes/Registro5.js
@@ -9,49 +9,49 @@ function Registro5(){
             <h1>DEDUCCIONES</h1>
             <br/>
             <h3>1 TIPO DE REGISTRO</h3>
-            <NumberInput/>
+            <NumberInput value="05" readOnly={true}/>
             <h3>2 CUIL</h3>
-            <NumberInput/>
+            <NumberInput maxLength={11}/>
             <h3>3 GANANCIA NO IMPONIBLE</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>4 DEDUCCIÓN ESPECIAL</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>5 DEDUCCIÓN ESPECIFICA</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>6 CÓNYUGE / UNIÓN CONVIVENCIAL</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>7 CANTIDAD DE HIJOS / HIJASTROS al 50%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <h3>8 HIJOS / HIJASTROS ($)</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>9 TOTAL DE CARGAS DE FAMILIA</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>10 TOTAL DEDUCCIONES ARTICULO 30</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>11 REMUNERACIÓN SUJETA A IMPUESTO (antes de deduccion Articulo 46 Ley 27.541)</h3>
-            <NumberInput/>
+            <NumberInput value="0" readOnly={true}/>
             <h3>12 DEDUCCIÓN INCISO A) ARTICULO 46 Ley 27.541 (GNI)</h3>
-            <NumberInput/>
+            <NumberInput value="0" readOnly={true}/>
             <h3>13 DEDUCCIÓN INCISO C) ARTICULO 46 Ley 27.541 (DE)</h3>
-            <NumberInput/>
+            <NumberInput value="0" readOnly={true}/>
             <h3>14 REMUNERACIÓN SUJETA A IMPUESTO</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>15 CANTIDAD DE HIJOS / HIJASTROS INCAPACITADOS PARA EL TRABAJO al 50%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <h3>16 HIJOS / HIJASTROS INCAPACITADOS PARA EL TRABAJO ($)</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>17 Deduccion Especial Incrementada Primera parte del penúltimo párrafo del inciso c) del articulo 30 de la ley del gravamen</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>18 Deduccion Especial Incrementada Segunda parte del penúltimo párrafo del inciso c) del articulo 30 de la ley del gravamen</h3>
-            <NumberInput/>
+            <NumberInput maxLength={15}/>
             <h3>19 CANTIDAD DE HIJOS / HIJASTROS al 100%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <h3>20 CANTIDAD DE HIJOS / HIJASTROS INCAPACITADOS al 100%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <h3>21 CANTIDAD DE HIJOS ENTRE 18 Y 24 AÑOS - EDUCACIÓN al 50%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <h3>22 CANTIDAD DE HIJOS ENTRE 18 Y 24 AÑOS - EDUCACIÓN al 100%</h3>
-            <NumberInput/>
+            <NumberInput maxLength={2}/>
             <br/>
         </>
     )
--- src/Routes/Registro6.js
+++ src/Routes/Registro6.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import NumberInput from "../Components/NumberInput";
 import SelectInput from "../Components/SelectInput";
 import OptionInput from "../Components/OptionInput";
 
 function Registro6(){
+    const [alicuotasArt, setAlicuotasArt] = useState('');
+    
+        const handleArtChange = (e) => {
+            setAlicuotasArt(e.target.value);
+        };
+
+    const [alicuotasApl, setAlicuotasApl] = useState('');
+    
+        const handleAplChange = (e) => {
+            setAlicuotasApl(e.target.value);
+        };
+
     return(
         <>
         <h2>REGISTRO 6</h2>
@@ -11,65 +23,67 @@ function Registro6(){
         <h1>CALCULO DE IMPUESTO</h1>
         <br/>
         <h3>1 TIPO DE REGISTRO</h3>
-        <NumberInput/>
+        <NumberInput value="06" readOnly={true}/>
         <h3>2 CUIL</h3>
-        <NumberInput/>
+        <NumberInput maxLength={11}/>
         <h3>3 ALÍCUOTA - ARTICULO 94 LEY GANANCIAS</h3>
-        <SelectInput>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
+        <SelectInput value={alicuotasArt} onChange={handleArtChange}>
+            <OptionInput value="" label="Seleccionar..."/>
+            <OptionInput value="0" label="0%"/>
+            <OptionInput value="1" label="5%"/>
+            <OptionInput value="2" label="9%"/>
+            <OptionInput value="3" label="12%"/>
+            <OptionInput value="4" label="15%"/>
+            <OptionInput value="5" label="19%"/>
+            <OptionInput value="6" label="23%"/>
+            <OptionInput value="7" label="26%"/>
+            <OptionInput value="8" label="31%"/>
+            <OptionInput value="9" label="35%"/>
         </SelectInput>
         <h3>4 ALÍCUOTA APLICABLE (SIN INCLUIR HORAS EXTRAS)</h3>
-        <SelectInput>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
-            <OptionInput/>
+        <SelectInput value={alicuotasApl} onChange={handleAplChange}>
+            <OptionInput value="" label="Seleccionar..."/>
+            <OptionInput value="0" label="0%"/>
+            <OptionInput value="1" label="5%"/>
+            <OptionInput value="2" label="9%"/>
+            <OptionInput value="3" label="12%"/>
+            <OptionInput value="4" label="15%"/>
+            <OptionInput value="5" label="19%"/>
+            <OptionInput value="6" label="23%"/>
+            <OptionInput value="7" label="26%"/>
+            <OptionInput value="8" label="31%"/>
+            <OptionInput value="9" label="35%"/>
         </SelectInput>
         <h3>5 IMPUESTO DETERMINADO</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>6 IMPUESTO RETENIDO</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>7 PAGOS A CUENTA (Total)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>8 SALDO</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>9 PAGOS A CUENTA - Impuesto sobre créditos y débitos en cuenta bancaria</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>10 PAGOS A CUNETA - Percepciones / Retenciones aduaneras</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>11 PAGOS A CUENTA - - Resolución General (AFIP) 3819/2015 - Cancelaciones en Efectivo - Servicios en el exterior por medio de agencias de viajes y turismo</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>12 PAGOS A CUENTA - BONO - LEY 27.424</h3>
-        <NumberInput/>
+        <NumberInput value="0" readOnly={true}/>
         <h3>13 PAGO A CUENTA - LEY 27.541 Articulo 35 inciso a)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>14 PAGO A CUENTA - LEY 27.541 Articulo 35 inciso b)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>15 PAGO A CUENTA - LEY 27.541 Articulo 35 inciso c)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>16 PAGO A CUENTA - LEY 27.541 Articulo 35 inciso d)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>17 PAGO A CUENTA - LEY 27.541 Articulo 35 inciso e)</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>18 PAGOS A CUENTA - Impuesto sobre créditos y débitos - sobre movimientos de fondos propios o de terceros</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <h3>19 PAGOS A CUNETA - Resolución General (AFIP) 3819/2015 - Cancelaciones en Efectivo - Servicios de transporte terrestre, aéreo y por vía acuática de pasajeros con destino fuera del país</h3>
-        <NumberInput/>
+        <NumberInput maxLength={15}/>
         <br/>
         </>
     )
